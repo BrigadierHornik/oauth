066c1c3c09eeadd07010f8c1f61a0acf61dfb087
Brigadier <brigadier@brigadier.dev>
2023-08-29 09:11:45 +0200

    Creating new entity for "clients" or applications that will be using the oauth

---
+++ app/src/Entity/Clients.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClientsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ClientsRepository::class)]
+class Clients
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 256)]
+    private ?string $client_id = null;
+
+    #[ORM\Column(length: 256)]
+    private ?string $client_secret = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getClientId(): ?string
+    {
+        return $this->client_id;
+    }
+
+    public function setClientId(string $client_id): static
+    {
+        $this->client_id = $client_id;
+
+        return $this;
+    }
+
+    public function getClientSecret(): ?string
+    {
+        return $this->client_secret;
+    }
+
+    public function setClientSecret(string $client_secret): static
+    {
+        $this->client_secret = $client_secret;
+
+        return $this;
+    }
+}
